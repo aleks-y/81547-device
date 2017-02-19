6e5d5913e893c084f8d11397779fad0e349956c7
Aleksey Ustinov <ustin89@mail.ru>
2017-02-19 23:26:28 +0300

    Главная страница + модальные окна

---
+++ gulpfile.js
@@ -0,0 +1,80 @@
+"use strict";
+
+var gulp = require("gulp");
+var plumber = require("gulp-plumber");
+var del = require("del");
+var rename = require("gulp-rename");
+var less = require("gulp-less");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var mqpacker = require("css-mqpacker");
+var minify = require("gulp-csso");
+var server = require("browser-sync").create();
+var run = require("run-sequence");
+
+gulp.task("clean", function() {
+  return del("build");
+});
+
+gulp.task("copy", function() {
+  return gulp.src([
+    "fonts/**/*{.woff,.woff2}",
+    "img/**/*{.jpg,.png,.svg}",
+    "js/**/*.js",
+    "*.html"
+    ], {
+      base: "."
+    })
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("copyhtml", function() {
+  return gulp.src("*.html")
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("style", function() {
+  gulp.src("less/style.less")
+    .pipe(plumber())
+    .pipe(less())
+    .pipe(postcss([
+      autoprefixer({browsers: [
+        "last 1 version",
+        "last 2 Chrome versions",
+        "last 2 Firefox versions",
+        "last 2 Opera versions",
+        "last 2 Edge versions"
+      ]}),
+      mqpacker({
+        sort: true
+      })
+    ]))
+    .pipe(gulp.dest("build/css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"))
+    .pipe(server.stream());
+});
+
+gulp.task("serve", function() {
+  server.init({
+    server: "build",
+    notify: false,
+    open: true,
+    ui: false
+  });
+
+  gulp.watch("img/**/*{.jpg,.png,.svg}", ["copy"]);
+  gulp.watch("less/**/*.less", ["style"]);
+  gulp.watch("*.html", ["copyhtml"]);
+  gulp.watch("build/*.html").on("change", server.reload);
+});
+
+gulp.task("build", function(fn) {
+  run(
+      "clean",
+      "copy",
+      "style",
+      fn
+  );
+});
